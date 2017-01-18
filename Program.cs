33e45d4ae4325855d4ee080a8003376c2cd384c8
git2tan <git2tan@mail.ru>
2017-01-18 17:16:22 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lab2_TurchinArtem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
