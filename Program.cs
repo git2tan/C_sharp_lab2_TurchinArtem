40517004020a7590803e85a29f0773e72c27fae2
git2tan <git2tan@mail.ru>
2017-01-25 20:08:11 +0700

    v1.0

--- Program.cs
+++ Program.cs
@@ -23,6 +23,20 @@ namespace C_sharp_lab2_TurchinArtem
             myCargoCar.DriveOneMile(myCargoCarBentley);
             Console.ReadKey();
             //GUI.work();
+            Console.WriteLine("-----------------------------------------------------------");
+            CargoCar car1 = new CargoCar(Marks.bentley, 400, 12000000, 1);
+            CargoCar car2 = new CargoCar(Marks.mitsubishi, 150, 900000, 10);
+            car2.RepairNow("Ремонт подвески1", 12000);
+            car2.RepairNow("Ремонт подвески2", 17000);
+            car2.RepairNow("Перегретый двигатель", 100000);
+            Console.WriteLine(car1);
+            Console.WriteLine("- - - - - - - - - - - - - - - -");
+            Console.WriteLine(car2);
+            car2.ListOfRepairsDate[1].Works = "Новые работы";
+            car2.ListOfRepairsDate[0].Date = new DateTime(2018,10,11);
+            car2.ListOfRepairsDate[2].Cost = 9999;
+            Console.WriteLine("После изменения:\n"+car2);
+            Console.ReadLine();
         }
     }
 }
